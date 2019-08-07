62b3b8476791775a0441a828472dc3a89e062f9a
shailesh19 <beingshailesh19@gmail.com>
2019-08-07 15:44:27 +0530

    First Commit

---
+++ src/main/java/com/stackroute/trackservice/domain/Track.java
@@ -0,0 +1,24 @@
+package com.stackroute.trackservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+
+@NoArgsConstructor
+// creates default constructor
+@AllArgsConstructor
+// creates parameterized constructor with all properties
+@Data
+@Document(collection = "track")
+public class Track
+{
+    @Id
+    private int trackId;
+    private String trackName;
+    private String comments;
+}
---
+++ src/main/java/com/stackroute/trackservice/repository/TrackRepository.java
@@ -0,0 +1,13 @@
+package com.stackroute.trackservice.repository;
+import com.stackroute.trackservice.domain.Track;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TrackRepository extends MongoRepository<Track,Integer>
+{
+//    @Query(value = "select p from Track p where p.trackName like ?1 ")
+    List<Track> findByTrackName(String trackName);
+}
